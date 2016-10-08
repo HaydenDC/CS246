730f7824f6770db1cb3375c0800173605c74e409
Hayden <car12039@byui.edu>
2016-10-07 20:19:53 -0600

    HelloWorld in test

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,7 +2,7 @@
 class HelloWorld{
 public static void main(String[] args){
 	System.out.println("Hello world....again");
-	System.out.println("The next line");
+	System.out.println("Hello GitHub World");
 
    }
 }

df2d9e545ad8d926e37f520e19149caa60f3c0c8
Hayden <car12039@byui.edu>
2016-10-07 20:36:28 -0600

    Removed world

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,7 +1,7 @@
 
 class HelloWorld{
 public static void main(String[] args){
-	System.out.println("Hello world....again");
+	System.out.println("Hello....again");
 	System.out.println("The next line");
 
    }

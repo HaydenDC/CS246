1c84f4b982a3604d8d01142b89e3093ef5a07b2d
Hayden <car12039@byui.edu>
2016-10-07 20:53:54 -0600

    Master switch

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,9 +1,8 @@
 
 class HelloWorld{
 public static void main(String[] args){
-	System.out.println("Hello world....again");
+	System.out.println("Hello....again");
 	System.out.println("Hello GitHub World");
-	System.out.println("Testing...");
 
    }
 }
